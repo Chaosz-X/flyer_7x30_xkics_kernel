a3ef5fb45f7e89d4a46ef13e20a65c21757c0cd1
Chaosz-X <corepwn20@gmail.com>
2012-03-28 15:57:04 -0400

    initial commit

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 18 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#18 PREEMPT Tue Mar 27 16:41:21 EDT 2012"
+#define LINUX_COMPILE_TIME "16:41:21"
+#define LINUX_COMPILE_BY "akin"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "ubuntu-domain"
+#define LINUX_COMPILER "gcc version 4.7.0 20120229 (experimental) [gcc-linaro/4.7 revision 114971] (GCC) "
